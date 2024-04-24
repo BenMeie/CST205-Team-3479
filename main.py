45ec09cb50571224b47c5cc1bc2d1de6bac87973
BenMeie <crazycowmm@gmail.com>
2024-04-24 15:52:42 -0700

    implement basic flask functionality and simple file grabbing method working around webapp silliness

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# {
+#   name: The file name
+#   path: The file's full path
+#   existing_tags[]: An array of tags that already exist for the file, mostly in the comments
+#   transcription_info: information returned by the Whisper Library
+# }
+audio_files = []
+
 @app.route('/')
-def home():
-    return render_template('home.html')
+def start():
+    return render_template('start.html')
+
+@app.route('/browse')
+def browse():
+    return render_template('browse.html')
+
+@app.route('/details')
+def details():
+    return render_template('details.html')
+
+@app.route('/select_folder', methods=['POST'])
+def select_folder():
+    for name in os.listdir(request.json['folder']):
+        if name.endswith('.wav'):
+            audio_files.append({'name': name, 'path': request.json['folder'] + '/' + name})
+    return ('', 200)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
