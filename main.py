59e0f51909c87cd11dc0d96642fb4a05c6456174
BenMeie <crazycowmm@gmail.com>
2024-04-24 23:39:58 -0700

    set up custom audio player and controls

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import os
+import shutil
+import atexit
 from flask import Flask, render_template, request
+
+def cleanup():
+    shutil.rmtree('./static/audio')
+    
+atexit.register(cleanup)
+try: 
+    os.mkdir('./static/audio')
+except FileExistsError:
+    pass
 
 app = Flask(__name__)
 
@@ -17,7 +28,7 @@ def start():
 
 @app.route('/browse')
 def browse():
-    return render_template('browse.html')
+    return render_template('browse.html', audio_files=audio_files)
 
 @app.route('/details')
 def details():
@@ -26,10 +37,11 @@ def details():
 @app.route('/select_folder', methods=['POST'])
 def select_folder():
     for name in os.listdir(request.json['folder']):
-        if name.endswith('.wav'):
-            audio_files.append({'name': name, 'path': request.json['folder'] + '/' + name})
+        if name.endswith('.wav') or name.endswith('.mp3'):
+            shutil.copy(request.json['folder'] + '/' + name, './static/audio/')
+            audio_files.append({'name': name, 'path': request.json['folder'] + '/' + name, 'id': len(audio_files)})
     return ('', 200)
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
